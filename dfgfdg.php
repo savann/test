2033cad8aeaf6cd669bc317a1db24ef423ce379e
savann <savchenko.com@gmail.com>
2015-12-27 17:53:50 +0200

    tredsf

---
+++ dfgfdg.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Portege
+ * Date: 27.12.2015
+ * Time: 17:52
+ */
+
+echo 1;
--- qwery.php
+++ qwery.php
@@ -1,7 +1,3 @@
 gd fgd fgdfgdfg dfg
-''' df dfg sdfg df gsd fg sdfg sd fgs date dsfg sd fg
-d sdfg test sd fgsd gf sdfg sdf gsd fgsd 
-ljkhkjn
-kjkj
-fdfgd dgdfg
+''' df dfg sdfg df gsd fg sdfg sd fgs date dsfg sd fgdgdfg
 test
